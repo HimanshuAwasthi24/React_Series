02d019f925ca821298155dd0b009c280b5a64626
Himanshu Awasthi <83570704+HimanshuAwasthi24@users.noreply.github.com>
2023-11-14 18:47:56 +0000

    bgChanger project addded

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div></div>
+  )
+}
+
+export default App
